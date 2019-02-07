01de92f21c10db18d41ce2ced7992d7337496890
Marcus Berger <marcusvcberger@gmail.com>
2019-02-07 16:11:58 -0200

    Complete the test class for addFive

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,7 +1,18 @@
 package tudelft.gettingstarted;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
     }
+
+    @Test
+    public void addFiveToTwenty(){
+        int result = new GettingStarted().addFive(20);
+        Assertions.assertEquals(25, result);
+    }
+
+
 }
